f2cc17aa28410c8bd28c9c2470bf094b4c35b51d
Aaron <ajccool@icloud.com>
2023-10-09 15:27:28 -0700

    create init db script

--- server/sql/init.sql
+++ server/sql/init.sql
@@ -1,5 +1,17 @@
-CREATE TABLE Test (
-    PersonID int,
-    LastName varchar(255),
-    FirstName varchar(255)
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE Metadata (
+    Id uuid DEFAULT uuid_generate_v4 () PRIMARY KEY NOT NULL,
+    Thumbnail varchar(255),
+    Title varchar(255),
+    Description varchar(255),
+    ReleaseDate varchar(255)
+);
+
+CREATE TABLE Movies (
+    Id uuid DEFAULT uuid_generate_v4 () PRIMARY KEY NOT NULL,
+    FileName varchar(255) NOT NULL,
+    MetadataId uuid,
+    FOREIGN KEY (MetadataId) REFERENCES Metadata(Id)
+        ON UPDATE CASCADE
 );
