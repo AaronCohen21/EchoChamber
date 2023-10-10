f2141168d17b55df586cd27150cd5d6d96ef8002
Aaron <ajccool@icloud.com>
2023-10-09 22:34:55 -0700

    cleanup static/tmp dir on boot

--- server/server.js
+++ server/server.js
@@ -1,7 +1,14 @@
+const fs = require('fs');
 const path = require('path');
 require('dotenv').config({ path: path.resolve(`${__dirname}/../.env`) });
 
-// TODO: each time before booting the static/tmp directory should be cleared out
+// Each time before booting the static/tmp directory should be cleared out
+const TMP_DIR = path.resolve(`${__dirname}/static/tmp`);
+fs.rmSync(TMP_DIR, {
+  recursive: true,
+  force: true,
+});
+fs.mkdirSync(TMP_DIR);
 
 const express = require('express');
 const cors = require('cors');
